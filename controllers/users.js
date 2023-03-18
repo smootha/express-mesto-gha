b0f6d064910654b28f4ab036f1d35ce1b8abadc8
Eugene Dovganich <passionuberalles@yandex.ru>
2023-03-18 18:57:39 +0300

    feat: add additional user features 2

--- controllers/users.js
+++ controllers/users.js
@@ -1,16 +1,19 @@
 const User = require('../models/user');
+
 // Получение всех пользователей
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
     .catch(() => res.status(500).send({ message: 'Ошибочка вышла! Попробуйте еще раз!' }));
 };
+
 // Получение пользователя по ID
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.id)
     .then((user) => res.send({ data: user }))
     .catch(() => res.status(500).send({ message: 'Ошибочка вышла! Попробуйте еще раз!' }));
 };
+
 // Создание пользователя
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
@@ -19,6 +22,7 @@ module.exports.createUser = (req, res) => {
     .then((user) => res.send({ data: user }))
     .catch(() => res.status(500).send({ message: 'Ошибочка вышла! Попробуйте еще раз!' }));
 };
+
 // Обновление профиля пользователя
 module.exports.updateProfile = (req, res) => {
   const { name, about } = req.body;
@@ -27,6 +31,7 @@ module.exports.updateProfile = (req, res) => {
     .then((user) => res.send({ data: user }))
     .catch(() => res.status(500).send({ message: 'Ошибочка вышла! Попробуйте еще раз!' }));
 };
+
 // Обновление аватара пользователя
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
