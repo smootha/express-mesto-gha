6cf05ea6544d9fc8337d83c0ce6dbd82b1062b25
Eugene Dovganich <passionuberalles@yandex.ru>
2023-03-20 03:26:24 +0300

    fix: replace body-parser

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const path = require('path');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+// eslint-disable-next-line import/no-extraneous-dependencies
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(express.static(path.join(__dirname, 'public')));
