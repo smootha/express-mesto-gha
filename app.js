ef4d33cc8a63d4038fb6678614395f2be4915ea1
Eugene Dovganich <passionuberalles@yandex.ru>
2023-03-21 16:20:01 +0300

    feat: add scurity npms(helmet + expressRateLimit)

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const bodyParser = require('body-parser');
+// Ограничение колличества запросов
+// eslint-disable-next-line import/no-extraneous-dependencies
+const rateLimit = require('express-rate-limit');
+// Защита от вэб уязвимостей
+// eslint-disable-next-line import/no-extraneous-dependencies
+const helmet = require('helmet');
+
+const { NOT_FOUND } = require('./utils/utils');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-// eslint-disable-next-line import/no-extraneous-dependencies
-const bodyParser = require('body-parser');
-const { NOT_FOUND } = require('./utils/utils');
 
+const apiLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: 'С вашего IP поступило слишком много запросов на сервер. Попробуйте через 5 минут',
+});
+
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(apiLimiter);
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
