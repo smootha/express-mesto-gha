e54ed9532da0f005163b254228543890485bf526
Eugene Dovganich <passionuberalles@yandex.ru>
2023-04-21 15:51:44 +0300

    fix: port to 3000

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 // С рассчетом на будущее сделал запрос значений из несуществуюшего .env файла
-const { PORT = '3001' } = process.env;
+const { PORT = '3000' } = process.env;
 const { JWT_SECRET = '944da6c9e36606d6274af0be02b1c9217eba92989c35a24d9e5ce517262f72cb' } = process.env;
 const { DB_ADRESS = 'mongodb://localhost:27017/mestodb' } = process.env;
 
