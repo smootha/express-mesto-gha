eac5ff2439cce7d5e25a66ac6b993781e7535c44
Eugene Dovganich <passionuberalles@yandex.ru>
2023-05-11 16:26:08 +0300

    feat: add request&error loggers in app.js

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const { PORT, DB_ADRESS } = require('./config');
 
 const { auth } = require('./middlewares/auth');
 const { errorHandler } = require('./middlewares/error-handler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { NotFoundError } = require('./middlewares/NotFoundError');
 // Настройки лимитера запросов
@@ -28,21 +29,25 @@ app.use(cors({ origin: `http://localhost:${PORT}` }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(apiLimiter);
-
+// Подключение к базе данных
 mongoose.connect(DB_ADRESS);
-
+// Подключение логгера запросов
+app.use(requestLogger);
+// Роуты доступные до авторизации
 app.use('/signup', require('./routes/signup'));
 app.use('/signin', require('./routes/signin'));
-
 // Авторизация
 app.use(auth);
 // Роуты доступные после успешной авторизации
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
-
+// Обработка некорректных роутов
 app.use('*', () => {
   throw new NotFoundError('Страница не найдена!');
 });
+
+// Подключение логгера ошибок
+app.use(errorLogger);
 // Обработчик ошибок celebrate
 app.use(errors());
 // Централизованный обработчик ошибок
